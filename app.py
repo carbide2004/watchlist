967e333018ffaa303bc779c0065981fbc1f65c67
steven_cnyali <957091215@qq.com>
2022-12-07 21:37:28 +0800

    Create, edit and delete item by form

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import os
 import sys
 import click
 
+from flask import request, url_for, redirect, flash
 from flask import Flask
 from flask import render_template
 from flask_sqlalchemy import SQLAlchemy
@@ -10,6 +11,7 @@ app = Flask(__name__)
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////' + os.path.join(app.root_path, 'data.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SECRET_KEY'] = 'dev'  # 等同于 app.secret_key = 'dev'
 
 db = SQLAlchemy(app)  # 初始化扩展，传入程序实例 app
 
@@ -18,11 +20,65 @@ def inject_user():  # 函数名可以随意修改
     user = User.query.first()
     return dict(user = user)  # 需要返回字典，等同于 return {'user': user}
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
     # user = User.query.first()
+    if request.method == 'POST':  # 判断是否是 POST 请求
+        # 获取表单数据
+        title = request.form.get('title')  # 传入表单对应输入字段的 name 值
+        year = request.form.get('year')
+        submit = request.form.get('submit')
+        # 验证数据
+        if not title or not year or len(year) > 4 or len(title) > 60:
+            flash('Invalid input.')  # 显示错误提示
+            return redirect(url_for('index'))  # 重定向回主页
+        # 保存表单数据到数据库
+        if submit == "Add":
+            movie = Movie(title=title, year=year)  # 创建记录
+            db.session.add(movie)  # 添加到数据库会话
+            db.session.commit()  # 提交数据库会话
+            flash('Item created.')  # 显示成功创建的提示
+        elif submit == "Delete":
+            movie = Movie.query.filter(Movie.title == title and Movie.year == year).first()
+            if (not movie):
+                flash('Invalid input.')  # 显示错误提示
+                return redirect(url_for('index'))  # 重定向回主页
+            db.session.delete(movie)
+            db.session.commit()  # 提交数据库会话
+            flash('Item deleted.')  
+        return redirect(url_for('index'))  # 重定向回主页
+    
     movies = Movie.query.all()
     return render_template('index.html', movies=movies)
+
+@app.route('/movie/edit/<int:movie_id>', methods=['GET', 'POST'])
+def edit(movie_id):
+    movie = Movie.query.get_or_404(movie_id)
+
+    if request.method == 'POST':  # 处理编辑表单的提交请求
+        title = request.form['title']
+        year = request.form['year']
+
+        if not title or not year or len(year) != 4 or len(title) > 60:
+            flash('Invalid input.')
+            return redirect(url_for('edit', movie_id=movie_id))  # 重定向回对应的编辑页面
+
+        movie.title = title  # 更新标题
+        movie.year = year  # 更新年份
+        db.session.commit()  # 提交数据库会话
+        flash('Item updated.')
+        return redirect(url_for('index'))  # 重定向回主页
+
+    return render_template('edit.html', movie=movie)  # 传入被编辑的电影记录
+
+@app.route('/movie/delete/<int:movie_id>', methods=['POST'])  # 限定只接受 POST 请求
+def delete(movie_id):
+    movie = Movie.query.get_or_404(movie_id)  # 获取电影记录
+    db.session.delete(movie)  # 删除对应的记录
+    db.session.commit()  # 提交数据库会话
+    flash('Item deleted.')
+    return redirect(url_for('index'))  # 重定向回主页
+
 
 @app.cli.command()  # 注册为命令，可以传入 name 参数来自定义命令
 @click.option('--drop', is_flag=True, help='Create after drop.')  # 设置选项
